7e01547adc42b55b68571ae793df6f7ea6185883
rutujaa121 <rutujaaugale@gmail.com>
2025-08-27 08:46:05 -0700

    added admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,31 @@
-const express = require("express");
-let bodyParser = require('body-parser');
+// const express = require("express");
+// let bodyParser = require('body-parser');
+// const router = express.Router();
+// let exe = require("../database/db.js");
+// // let isAuthenticate = require("../middleware/isAuth.js");
+// 
+// router.use(bodyParser.urlencoded({extended : true}));
+// 
+// 
+// router.get("/", function(req, res){
+//     res.render("admin/admin.ejs");
+// });
+
+
+import express from "express";
+import { db } from "../database/db.js";
+
 const router = express.Router();
-let exe = require("../database/db.js");
-// let isAuthenticate = require("../middleware/isAuth.js");
 
-router.use(bodyParser.urlencoded({extended : true}));
-
-
-router.get("/", function(req, res){
-    res.render("admin/admin.ejs");
+// Admin Dashboard
+router.get("/dashboard", async (req, res) => {
+  try {
+    const [rows] = await db.query("SELECT * FROM applications");
+    res.render("admin/admin", { applications: rows });
+  } catch (err) {
+    console.error(err);
+    res.send("Error loading applications");
+  }
 });
+
+export default router;
