9a2a6f1cfe46ed1cd936f4b44def890c02ee721e
rutujaa121 <rutujaaugale@gmail.com>
2025-08-27 08:43:15 -0700

    added user

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,40 @@
-const express = require("express");
+// const express = require("express");
+// const router = express.Router();
+// let exe = require("../database/db.js");
+// 
+// // router.get("/",async function(req, res){
+// //     let data = await exe('SELECT * FROM tbl2_home')
+// //     res.render("user/home.ejs", {homedata : data[0]});
+// // });
+// 
+// module.exports = router;
+
+
+import express from "express";
+import { db } from "../database/db.js";
+
 const router = express.Router();
-let exe = require("../database/db.js");
 
-// router.get("/",async function(req, res){
-//     let data = await exe('SELECT * FROM tbl2_home')
-//     res.render("user/home.ejs", {homedata : data[0]});
-// });
+// Admission Form - GET
+router.get("/admission", (req, res) => {
+  res.render("user/admission");
+});
 
-module.exports = router;
+// Admission Form - POST
+router.post("/admission", async (req, res) => {
+  const { name, email, dob, course, category, score } = req.body;
+
+  try {
+    const [result] = await db.query(
+      "INSERT INTO applications (name, email, dob, course, category, score) VALUES (?,?,?,?,?,?)",
+      [name, email, dob, course, category, score]
+    );
+
+    res.render("user/success", { appId: result.insertId });
+  } catch (err) {
+    console.error(err);
+    res.send("Error submitting form");
+  }
+});
+
+export default router;
