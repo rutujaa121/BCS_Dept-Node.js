d8df9ccbde7cf7d6be821a9dfb06e6d05d4d6416
rutujaa121 <rutujaaugale@gmail.com>
2025-09-06 23:32:03 -0700

    add header

--- routes/user.js
+++ routes/user.js
@@ -13,6 +13,11 @@ router.get("/about", (req, res) => {
   res.render("user/about", { title: "About Us" });
 });
 
+// Admission page
+router.get("/admission", (req, res) => {
+  res.render("user/admission", { title: "Admission" });
+});
+
 // Notice page
 router.get("/notice", (req, res) => {
   res.render("user/notice", { title: "Notice" });
